8b318e753b63281bbe5569274c7b850fb1e1e152
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2024-12-10 15:30:28 +0000

    Created

--- src/main/java/com/shoplify/shoplify/api/controller/product/ProductController.java
+++ src/main/java/com/shoplify/shoplify/api/controller/product/ProductController.java
@@ -1,27 +1,54 @@
 package com.shoplify.shoplify.api.controller.product;
 
+import com.shoplify.shoplify.api.model.ProductDetails;
 import com.shoplify.shoplify.models.Product;
 import com.shoplify.shoplify.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
     public ProductController(ProductService productService) {
         this.productService = productService;
-
     }
-
 
     @GetMapping("/")
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    public ResponseEntity<Page<Product>> getProducts(@RequestParam(defaultValue = "0" ) int page, @RequestParam(defaultValue = "10") int size
+            , @RequestParam(defaultValue = "id") String sort, @RequestParam(defaultValue = "asc") String order) {
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.valueOf(order), sort);
+        Page<Product> products = productService.getProducts(pageable);
+
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{product_id}")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable Long product_id){
+        return ResponseEntity.ok(productService.getProduct(product_id));
+    }
+
+    @PutMapping("/{product_id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long product_id, @RequestBody ProductDetails productDetails) {
+        productService.updateProduct(productDetails, product_id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{product_id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long product_id) {
+        productService.deleteProduct(product_id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
+
